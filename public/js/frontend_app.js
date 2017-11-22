676f21bf84621cb58804f00a2b6a8dd9a71635f4
Hagil Baik <hagilbaik@gmail.com>
2017-11-22 11:50:26 +0800

    "You know, we've won awards for this crap." -- David Letterman

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,1 +1,50 @@
 console.log('loaded frontend_app');
+
+var frontend_app = angular.module('employees', []);
+frontend_app.controller('data', data_stuff);
+
+function data_stuff($scope, $http) {
+    console.log('inside datastuff');
+    $scope.message = 'howdy';
+    $scope.read = function () {
+        console.log('reading data from backend');
+        $http.get('/api/v3/read').then(function (employees){
+           // console.log(employees);
+            $scope.employees = employees.data;
+        });
+    }
+    $scope.read();
+
+    $scope.create = function () {
+        console.log('creating employee');
+        var data = {
+            name: $scope.input_name,
+            gender: $scope.input_gender,
+            job: $scope.input_job
+        }
+        $http.post('/api/v3/create', data).then(function (result){
+            console.log(result);
+            $scope.message = result.data.message;
+        });
+        $scope.read();
+    }
+
+    $scope.update = function (employee) {
+        console.log('updating employee');
+        var data = {
+            _id: employee._id,
+            name: employee.name,
+            gender: employee.gender,
+            job: employee.job
+        }
+        $http.put('/api/v3/update', data).then();
+        $scope.read();
+    }
+
+    $scope.delete = function (employee) {
+        console.log('deleting employee');
+        $http.delete('/api/v3/delete/' + employee._id).then();
+        $scope.read();
+    }
+
+}
